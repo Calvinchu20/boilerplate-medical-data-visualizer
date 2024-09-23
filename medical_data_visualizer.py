d239039109c36b84de7fa660c72846ff8ca1aa7e
Calvin  Chu <cchu9826@gmail.com>
2024-09-23 20:50:02 +0000

    deat

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+def isOver(d):
+    if d > 25:
+        return 1
+    else:
+        return 0
+df['overweight'] =  df['weight']/(df['height']/100)**2    
+df['overweight'] = df['overweight'].apply(isOver)
 
 # 3
+def normalize(num):
+    if num >1:
+        return 1
+    else:
+        return 0
 
+df['gluc'] = df['gluc'].apply(normalize)
+df['cholesterol'] =df['cholesterol'].apply(normalize)
 
 # 4
 def draw_cat_plot():
+
     # 5
-    df_cat = None
+    df_cat = pd.melt(df,id_vars = ['cardio'],value_vars=['active','alco','cholesterol','gluc','overweight','smoke'],var_name = 'feature',value_name = 'value')
 
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby(['cardio','feature','value']).size().reset_index(name = "total")
+    print("cat", df_cat)
 
     # 7
+    df_cat = pd.melt(df,id_vars = ['cardio'],value_vars=['active','alco','cholesterol','gluc','overweight','smoke'],var_name = 'variable',value_name = 'value')
+    graphs = sns.catplot(x='variable', hue="value", col="cardio", data=df_cat, kind="count" )
+    graphs.set(ylabel = 'total')
+
 
 
 
     # 8
-    fig = None
+    fig = graphs
 
 
     # 9
